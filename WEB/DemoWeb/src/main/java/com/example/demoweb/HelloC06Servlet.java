ad24e85150c9c8756d02eec7992a662a74517176
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-11-17 19:42:39 +0700

    demo web

---
+++ WEB/DemoWeb/src/main/java/com/example/demoweb/HelloC06Servlet.java
@@ -0,0 +1,47 @@
+package com.example.demoweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet(name = "helloC06Servlet", value = "/home")
+public class HelloC06Servlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        List<String> strings = new ArrayList<>();
+//        strings.add("Đề");
+//        strings.add("Dân");
+//        strings.add("Tiến Anh");
+//        response.setContentType("text/html");
+//        PrintWriter printWriter = response.getWriter();
+//        String str = "";
+//        for (String s: strings) {
+//            str += "<h1>" + s + "</h1>";
+//        }
+//        printWriter.write(str);
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.write("<form action=\"/home\" method=\"post\">\n" +
+                "    <input type=\"text\" name=\"nameUser\">\n" +
+                "    <button>Nhấn</button>\n" +
+                "</form>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("nameUser");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("<h1>" + name + "</h1>");
+    }
+}
+
+// Tạo một lớp Student (id, name, image) sau đó hiển thị chúng lên web;
---
+++ WEB/DemoWeb/src/main/java/com/example/demoweb/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example.demoweb;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "Hello" + "</h1>");
+        out.println("</body></html>");
+    }
+
+}
